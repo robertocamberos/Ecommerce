b1d803e4d64271ba78ad2cc7720363c561a1545f
Roberto Camberos <42880184+rcamberos23@users.noreply.github.com>
2022-10-14 13:19:00 -0700

    CreateReactApp

---
+++ test.js
@@ -0,0 +1,17 @@
+import React from "react";
+import second from 'first'
+
+import React from 'react'
+
+export default function test() {
+  return (
+    <div>test</div>
+  )
+}
+
+function App() {
+  return <div className="test"> hello world</div>;
+}
+
+export default App;
+
